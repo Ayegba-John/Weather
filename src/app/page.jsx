e250e056b38412ae69abfa64a890aa913b687001
Ayegba John <ayegbajohn@gmail.com>
2025-01-14 03:33:59 +0100

    basic set up

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+const page = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.wrapper}>waether app</div>
+    </div>
+  );
+};
+export default page;
